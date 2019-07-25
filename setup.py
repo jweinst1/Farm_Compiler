0705a4fb0be1a9c1dbaafc3bb994ad60c2a2f437
timoeller <timo.moeller@deepset.ai>
2019-07-25 09:58:45 +0200

    Update download url to new release in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
   author = 'Joshua Weinstein',
   author_email = 'jweinst1@berkeley.edu',
   url = 'https://github.com/jweinst1/Farm_Compiler', # use the URL to the github repo
-  download_url = 'https://github.com/jweinst1/Farm_Compiler/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/Timoeller/Farm_Compiler/archive/1.0.1.tar.gz', # I'll explain this in a second
   keywords = ['compilers', 'programming', 'farm_compiler', 'farm'], # arbitrary keywords
   classifiers = [],
 )
